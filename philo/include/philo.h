656360cae5e945290097e1bde7d55861ef2ce5d8
Gabriel MANSUY <gmansuy@e1r6p7.clusters.42paris.fr>
2022-09-25 17:07:35 +0200

    changed almost nothing

---
+++ philo/include/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmansuy <gmansuy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 15:51:43 by gmansuy           #+#    #+#             */
+/*   Updated: 2022/09/25 17:02:56 by gmansuy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_data
+{
+}	t_data;
+
+
+#endif
