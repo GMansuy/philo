1eb3d722d139fbf2f62698abd0041cfee064184f
Gabriel MANSUY <gmansuy@e1r5p18.clusters.42paris.fr>
2022-10-05 17:06:19 +0200

    Checker les data race

--- philo/obj/0_utils/ft_itoa.d
+++
@@ -1,4 +0,0 @@
-obj/0_utils/ft_itoa.o: src/0_utils/ft_itoa.c \
-  src/0_utils/../../include/philo.h
-
-src/0_utils/../../include/philo.h:
--- philo/obj/0_utils/ft_strjoin.d
+++
@@ -1,4 +0,0 @@
-obj/0_utils/ft_strjoin.o: src/0_utils/ft_strjoin.c \
-  src/0_utils/../../include/philo.h
-
-src/0_utils/../../include/philo.h:
